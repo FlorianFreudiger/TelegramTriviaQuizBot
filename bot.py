be34969e2155520eb42b9b62f8899d0c7aa8e1c3
Florian Freudiger <freudiger.florian@gmail.com>
2020-02-01 20:18:42 +0100

    Bot and quiz

---
+++ bot.py
@@ -0,0 +1,130 @@
+import logging
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
+import quiz
+import secret
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO, filename='log.log')  # Enable logging
+logging.debug("Logging enabled")
+
+
+# InlineKeyboards:
+
+def CatIButton(category) -> InlineKeyboardButton:
+    return InlineKeyboardButton(str(category), callback_data="category:"+category.name)
+
+category_keyboard = [[CatIButton(quiz.QuizCategory.ANY_CATEGORY)],
+                     [CatIButton(quiz.QuizCategory.GENERAL_KNOWLEDGE), CatIButton(
+                         quiz.QuizCategory.SCIENCE_AND_NATURE), CatIButton(quiz.QuizCategory.MYTHOLOGY)],
+                     [CatIButton(quiz.QuizCategory.SCIENCE_COMPUTERS), CatIButton(
+                         quiz.QuizCategory.SCIENCE_MATHEMATICS), CatIButton(quiz.QuizCategory.SCIENCE_GADGETS)],
+                     [CatIButton(quiz.QuizCategory.ENTERTAINMENT_BOOKS), CatIButton(
+                         quiz.QuizCategory.ENTERTAINMENT_FILM), CatIButton(quiz.QuizCategory.ENTERTAINMENT_MUSIC)],
+                     [CatIButton(quiz.QuizCategory.ENTERTAINMENT_CARTOON_AND_ANIMATIONS), CatIButton(
+                         quiz.QuizCategory.ENTERTAINMENT_JAPANESE_ANIME_AND_MANGA), CatIButton(quiz.QuizCategory.ENTERTAINMENT_MUSICALS_AND_THEATRES)],
+                     [CatIButton(quiz.QuizCategory.ENTERTAINMENT_BOARD_GAMES), CatIButton(
+                         quiz.QuizCategory.ENTERTAINMENT_VIDEO_GAMES), CatIButton(quiz.QuizCategory.ENTERTAINMENT_TELEVISION)],
+                     [CatIButton(quiz.QuizCategory.ENTERTAINMENT_COMICS), CatIButton(
+                         quiz.QuizCategory.ART), CatIButton(quiz.QuizCategory.CELEBRITIES)],
+                     [CatIButton(quiz.QuizCategory.GEOGRAPHY), CatIButton(
+                         quiz.QuizCategory.HISTORY), CatIButton(quiz.QuizCategory.POLITICS)],
+                     [CatIButton(quiz.QuizCategory.SPORTS), CatIButton(quiz.QuizCategory.ANIMALS), CatIButton(quiz.QuizCategory.VEHICLES)]]
+category_reply_markup = InlineKeyboardMarkup(
+    category_keyboard, one_time_keyboard=True)
+
+
+difficulty_keyboard = [[InlineKeyboardButton("Any Difficulty", callback_data="difficulty:-1")],
+                       [InlineKeyboardButton("Easy", callback_data="difficulty:0"), InlineKeyboardButton("Medium", callback_data="difficulty:1"), InlineKeyboardButton("Hard", callback_data="difficulty:2")]]
+difficulty_reply_markup = InlineKeyboardMarkup(
+    difficulty_keyboard, one_time_keyboard=True)
+
+
+# Commands:
+
+def command_start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        'Hello!\nUse /quiz to start generate a new quiz.\nSettings: /category and /difficulty')
+
+
+def command_category(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        'Please choose category:', reply_markup=category_reply_markup)
+
+
+def command_difficulty(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        'Please choose difficulty:', reply_markup=difficulty_reply_markup)
+
+
+def command_quiz(update: Update, context: CallbackContext):
+    if "category" in context.chat_data:
+        category = context.chat_data["category"]
+    else:
+        category = quiz.QuizCategory.ANY_CATEGORY
+
+    if "difficulty" in context.chat_data:
+        difficulty = context.chat_data["difficulty"]
+    else:
+        difficulty = quiz.QuizDifficulty.ANY
+
+    success, newQuiz = quiz.Quiz.fromInternet(
+        category=category, difficulty=difficulty)
+    if (success):
+        answers, correctIndex = newQuiz.getAnswers()
+        # https://python-telegram-bot.readthedocs.io/en/stable/telegram.bot.html#telegram.Bot.send_poll
+        pollMessage = context.bot.send_poll(
+            update.effective_chat.id, newQuiz.question, answers)
+        # Quizzes are not yet availibe in the python-telegram-bot api, for now just send the solution after 90 seconds
+        context.job_queue.run_once(delayedReplyMessage, 90, context=[
+                                   pollMessage, "Correct answer: "+str(correctIndex+1)+". option"])
+
+    else:
+        update.message.reply_text('Error, cannot create poll.')
+
+
+def delayedReplyMessage(context: CallbackContext):
+    context.job.context[0].reply_text(context.job.context[1])
+
+
+# Callback queries:
+
+def callbackQueryFunction(update: Update, context: CallbackContext):
+    query = update.callback_query
+
+    if query.data.startswith("category:"):
+        category = quiz.QuizCategory[query.data[9:]]
+        context.chat_data["category"] = category
+        query.edit_message_text("Quiz category set to "+str(category))
+
+    elif query.data.startswith("difficulty:"):
+        difficulty = quiz.QuizDifficulty(int(query.data[11:]))
+        context.chat_data["difficulty"] = difficulty
+        query.edit_message_text("Quiz difficulty set to "+str(difficulty))
+
+    else:
+        logging.warning("Unknown callback query: "+query.data)
+        query.answer()
+
+
+logging.info("Starting bot...")
+# Create the EventHandler and pass it your bot's token.
+updater = Updater(token=secret.api_key(), use_context=True)
+
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
+
+# command handlers
+dispatcher.add_handler(CommandHandler("start", command_start))
+dispatcher.add_handler(CommandHandler("quiz", command_quiz))
+dispatcher.add_handler(CommandHandler("category", command_category))
+dispatcher.add_handler(CommandHandler("difficulty", command_difficulty))
+dispatcher.add_handler(CallbackQueryHandler(callbackQueryFunction))
+
+# Start the Bot
+updater.start_polling()
+
+# Run the bot until you press Ctrl-C or the process receives SIGINT,
+# SIGTERM or SIGABRT. This should be used most of the time, since
+# start_polling() is non-blocking and will stop the bot gracefully.
+updater.idle()
---
+++ quiz.py
@@ -0,0 +1,165 @@
+import logging
+import json
+import html
+import random
+import urllib.request
+from enum import Enum
+
+
+class QuizDifficulty(Enum):
+    ANY = -1
+    EASY = 0
+    MEDIUM = 1
+    HARD = 2
+
+    def __str__(self):
+        if self == QuizDifficulty.ANY:
+            return "Any"
+        elif self == QuizDifficulty.EASY:
+            return "Easy"
+        elif self == QuizDifficulty.MEDIUM:
+            return "Medium"
+        elif self == QuizDifficulty.HARD:
+            return "Hard"
+        else:
+            logging.error("QuizDifficulty is weird.")
+            return "ERROR"
+
+    def toUrlPart(self) -> str:
+        if self == QuizDifficulty.ANY:
+            return ""
+        elif self == QuizDifficulty.EASY:
+            return "&difficulty=easy"
+        elif self == QuizDifficulty.MEDIUM:
+            return "&difficulty=medium"
+        elif self == QuizDifficulty.HARD:
+            return "&difficulty=hard"
+        else:
+            logging.error("QuizDifficulty is weird.")
+            return ""
+
+
+class QuizCategory(Enum):
+    ANY_CATEGORY = 0
+    GENERAL_KNOWLEDGE = 9
+    ENTERTAINMENT_BOOKS = 10
+    ENTERTAINMENT_FILM = 11
+    ENTERTAINMENT_MUSIC = 12
+    ENTERTAINMENT_MUSICALS_AND_THEATRES = 13
+    ENTERTAINMENT_TELEVISION = 14
+    ENTERTAINMENT_VIDEO_GAMES = 15
+    ENTERTAINMENT_BOARD_GAMES = 16
+    SCIENCE_AND_NATURE = 17
+    SCIENCE_COMPUTERS = 18
+    SCIENCE_MATHEMATICS = 19
+    MYTHOLOGY = 20
+    SPORTS = 21
+    GEOGRAPHY = 22
+    HISTORY = 23
+    POLITICS = 24
+    ART = 25
+    CELEBRITIES = 26
+    ANIMALS = 27
+    VEHICLES = 28
+    ENTERTAINMENT_COMICS = 29
+    SCIENCE_GADGETS = 30
+    ENTERTAINMENT_JAPANESE_ANIME_AND_MANGA = 31
+    ENTERTAINMENT_CARTOON_AND_ANIMATIONS = 32
+
+    def __str__(self):
+        if self == QuizCategory.ANY_CATEGORY:
+            return "ANYTHING"
+        elif self == QuizCategory.GENERAL_KNOWLEDGE:
+            return "General knowledge"
+        elif self == QuizCategory.ENTERTAINMENT_BOOKS:
+            return "Books"
+        elif self == QuizCategory.ENTERTAINMENT_FILM:
+            return "Film"
+        elif self == QuizCategory.ENTERTAINMENT_MUSIC:
+            return "Music"
+        elif self == QuizCategory.ENTERTAINMENT_MUSICALS_AND_THEATRES:
+            return "Musicals & Theatres"
+        elif self == QuizCategory.ENTERTAINMENT_TELEVISION:
+            return "TV"
+        elif self == QuizCategory.ENTERTAINMENT_VIDEO_GAMES:
+            return "Video Games"
+        elif self == QuizCategory.ENTERTAINMENT_BOARD_GAMES:
+            return "Board Games"
+        elif self == QuizCategory.SCIENCE_AND_NATURE:
+            return "Science & Nature"
+        elif self == QuizCategory.SCIENCE_COMPUTERS:
+            return "Computers"
+        elif self == QuizCategory.SCIENCE_MATHEMATICS:
+            return "Mathematics"
+        elif self == QuizCategory.ENTERTAINMENT_COMICS:
+            return "Comics"
+        elif self == QuizCategory.SCIENCE_GADGETS:
+            return "Gadgets"
+        elif self == QuizCategory.ENTERTAINMENT_JAPANESE_ANIME_AND_MANGA:
+            return "Anime & Manga"
+        elif self == QuizCategory.ENTERTAINMENT_CARTOON_AND_ANIMATIONS:
+            return "Cartoon & Amimations"
+        else:
+            return self.name.capitalize()  # pylint: disable=no-member
+
+    def toUrlPart(self) -> str:
+        if self == QuizCategory.ANY_CATEGORY:
+            return ""
+        else:
+            return "&category="+str(self.value)
+
+
+class Quiz:
+    def __init__(self, category: QuizCategory, quizType: int, difficulty: QuizDifficulty, question: str, correctAnswer: str, incorrectAnswers):
+        self.category = category
+        self.quizType = quizType
+        self.difficulty = difficulty
+        self.question = question
+        self.correctAnswer = correctAnswer
+        self.incorrectAnswers = incorrectAnswers
+
+    @staticmethod
+    def fromInternet(category=QuizCategory.ANY_CATEGORY, difficulty=QuizDifficulty.ANY):
+        url = "https://opentdb.com/api.php?amount=1" + \
+            category.toUrlPart() + difficulty.toUrlPart()
+        logging.info("Requesting: "+url)
+
+        with urllib.request.urlopen(url) as url:
+            data = json.loads(url.read())
+
+        logging.info(data)
+        
+        responseCode = data['response_code']
+        if responseCode != 0:
+            logging.error("Response code: "+str(responseCode))
+            return False
+
+        result = data['results'][0]
+
+        # Unescape html characters
+        question = html.unescape(result['question'])
+        correct_answer = html.unescape(result['correct_answer'])
+        incorrect_answers = []
+        for incorrect_answer in result['incorrect_answers']:
+            incorrect_answers.append(html.unescape(incorrect_answer))
+
+        return True, Quiz(result['category'], result['type'], result['difficulty'], question, correct_answer, incorrect_answers)
+
+    def getAnswers(self):
+        if self.quizType == 'multiple':
+            answers = self.incorrectAnswers[:]
+            random.shuffle(answers)
+
+            correctIndex = random.randint(0, len(answers))
+            answers.insert(correctIndex, self.correctAnswer)
+            return answers, correctIndex
+
+        elif self.quizType == 'boolean':
+            answers = ["True", "False"]
+            if self.correctAnswer == 'True':
+                return answers, 0
+            else:
+                return answers, 1
+
+        else:
+            logging.error("quizType is neither 'multiple' nor 'boolean'")
